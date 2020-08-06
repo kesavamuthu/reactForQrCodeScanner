580375e215e83b11e48e4127a301da49d11d8e22
kesavamuthu <kesavamuthu77@gmail.com>
2020-08-07 02:35:18 +0530

    able to login

---
+++ src/components-init/input.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+function InputProvider(props) {
+  console.log(props);
+  let inputs = props.register[0];
+  let lables = props.register[1];
+  let placeHolders = props.register[2];
+  let datas = props.data;
+  let names = Object.keys(datas);
+  console.log(names);
+  return inputs.map((e, i) => (
+    <Form.Group controlId={e} key={i}>
+      <Form.Label>{lables[i]}</Form.Label>
+      <Form.Control
+        type={e}
+        placeholder={placeHolders[i]}
+        name={names[i]}
+        value={datas[names[i]]}
+        onChange={props.onChange}
+      />
+    </Form.Group>
+  ));
+}
+
+export default InputProvider;
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// const Connect = { url: "http://localhost:9000/" };
 const Connect = { url: "https://nameless-taiga-33242.herokuapp.com/" };
 Object.freeze(Connect);
 
